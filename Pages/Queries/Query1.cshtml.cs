4628f61f6aa6a29c3f6ea93a854806aa1facfb4c
nadav1996700 <nadav1996700@gmail.com>
2021-06-27 03:19:00 +0300

    create first query

---
+++ Pages/Queries/Query1.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TicTacToeServer.Data;
+using TicTacToeServer.Models;
+
+namespace TicTacToeServer.Pages.Players
+{
+    public class Query1Model : PageModel
+    {
+        private readonly MyContext _context;
+
+        public Query1Model(MyContext context)
+        {
+            _context = context;
+        }
+
+        public IList<TblPlayers> TblPlayers { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            TblPlayers = await _context.TblPlayers.ToListAsync();
+        }
+
+        public async Task OnPostAsync()
+        {
+            TblPlayers = await _context.TblPlayers.ToListAsync();
+        }
+    }
+}
