c2c1416fe6dd4ede8958eb040aa44ac58fc0e4b3
nadav1996700 <nadav1996700@gmail.com>
2021-08-18 17:15:12 +0300

    fix query 5

--- Pages/QueriesList/Query5.cshtml.cs
+++ Pages/QueriesList/Query5.cshtml.cs
@@ -28,13 +28,29 @@ namespace TicTacToeServer.Pages.QueriesList
         {
             List<TblGames> tblGames = await _context.TblGames.ToListAsync();
             TblPlayers = await _context.TblPlayers.ToListAsync();
+            // max count of games that one player played
+            int max = (from p in TblPlayers
+                       let count = tblGames.Count(g => g.PlayerId == p.Id)
+                       orderby count descending
+                       select count).First();
+            Groups = new Dictionary<int, List<TblPlayers>>();
 
-            Groups = (from p in TblPlayers
-                     let count = tblGames.Count(g => g.PlayerId == p.Id)
-                     orderby count descending
-                     group p by count into g
-                     select g)
-                    .ToDictionary(g => g.Key, g => g.ToList());
+            for (int i = max; i >= 0; i--)
+            {
+                List<TblPlayers> players =
+                    (from p in TblPlayers
+                    where tblGames.Count(g => g.PlayerId == p.Id) == i
+                    select p).ToList();
+                Groups.Add(i, players);
+            }
+
+            // code for display the results without gap of empty games
+            //Groups = (from p in TblPlayers
+            //         let count = tblGames.Count(g => g.PlayerId == p.Id)
+            //         orderby count descending
+            //         group p by count into g
+            //         select g)
+            //        .ToDictionary(g => g.Key, g => g.ToList());
         }
     }
 }
